603a793132401394a03cf5f60b1cab0c3ba3491a
Javi Corvi <javicorvi@gmail.com>
2019-02-13 14:38:41 +0100

    comments into file util

--- src/main/java/es/bsc/inb/etransafe/treatmentfindings/util/FileUtil.java
+++ src/main/java/es/bsc/inb/etransafe/treatmentfindings/util/FileUtil.java
@@ -6,9 +6,19 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-
+/**
+ * File Util 
+ * @author jcorvi
+ *
+ */
 public class FileUtil {
-
+	/**
+	 * Create a plain text file with the given string
+	 * @param path
+	 * @param plainText
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
 	public static void createTxtFile(String path, String plainText) throws FileNotFoundException, IOException {
 		File fout = new File(path);
 		FileOutputStream fos = new FileOutputStream(fout);
